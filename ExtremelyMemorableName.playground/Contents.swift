89f1908bec6709c75ddda85d42fea0234d20586d
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-07 20:36:58 -0400

    Finished lab

---
+++ ExtremelyMemorableName.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+let thirdRock = "Earth" //inferred string
+
+var ninthRock: String //declared string
+
+ninthRock = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+//fifthPlanet is a constant of type String
+
+let sixthPlanet: String = "Saturn"
+//sixthPlanet is a constant of type String
+
+let seventhPlanet = "Uranus"
+//seventhPlanet is a constant of type String
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
